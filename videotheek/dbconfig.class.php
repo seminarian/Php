496241c7cc12eaf1200d980780bce6fce13809e6
Pieter Doms <pdoms@telenet.be>
2015-01-29 15:55:46 +0100

    Het begin

---
+++ videotheek/dbconfig.class.php
@@ -0,0 +1,7 @@
+<?php
+	class DBConfig {
+	public static $DB_CONNSTRING = "mysql:host=localhost;dbname=cursusphp";
+	public static $DB_USERNAME = "root";
+	public static $DB_PASSWORD = "vdab";
+	}
+?>
